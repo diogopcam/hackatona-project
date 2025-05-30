f19674e046a748b826f632977bcd1efc32a6050a
Diogo <134295616+diogopcam@users.noreply.github.com>
2025-05-30 19:26:04 -0300

    Commit inicial

---
+++ hackatona-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hackatona-project
+//
+//  Created by Diogo Camargo on 30/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hackatona-projectTests/hackatona_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  hackatona_projectTests.swift
+//  hackatona-projectTests
+//
+//  Created by Diogo Camargo on 30/05/25.
+//
+
+import Testing
+@testable import hackatona_project
+
+struct hackatona_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
