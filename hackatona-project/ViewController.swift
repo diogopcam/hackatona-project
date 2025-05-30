8d05b059d252e284e978f462b8f6eee81c34a7da
Diogo <134295616+diogopcam@users.noreply.github.com>
2025-05-30 19:59:37 -0300

    Add files via upload

--- hackatona-project/ViewController.swift
+++ hackatona-project/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var label: UILabel = {
+        var text = UILabel()
+        text.text = "Hello world"
+        return text
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(label)
         // Do any additional setup after loading the view.
     }
-
-
 }
+
+
 
