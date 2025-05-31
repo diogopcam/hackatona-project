d8ed2334e68181626f9a84577be927b767e858d6
Mauricio Krziminski <mauricio.krziminski@edu.pucrs.br>
2025-05-31 05:35:04 -0300

    fix: tabBarController

--- hackatona-project/SceneDelegate.swift
+++ hackatona-project/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         let window = UIWindow(windowScene: windowScene)
         self.window = window
-        let tabBarController = LoginVC()
+        let tabBarController = TabBarController()
         window.rootViewController = tabBarController
 //        window.rootViewController = rootViewController
 //        window.rootViewController = ProductListVC()
