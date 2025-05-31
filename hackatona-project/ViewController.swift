77316d1a6d5cfac6b68654a95f4d4772e71f2f2b
Diogo Pessin Ca <diogpcam@Diogo-Camargo---MacBook-Pro.local>
2025-05-30 23:46:05 -0300

    Initial commit.

--- hackatona-project/ViewController.swift
+++ hackatona-project/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(label)
+        super.view.backgroundColor = .blue
         // Do any additional setup after loading the view.
     }
 }
