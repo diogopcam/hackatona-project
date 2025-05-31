610f36db723d0674ba2f5f3ef4a96c16b356f32f
Marcos <marcosraach.1@gmail.com>
2025-05-31 13:46:36 -0300

    feat: user model

--- hackatona-project/Model/User.swift
+++ hackatona-project/Model/User.swift
@@ -5,3 +5,26 @@
 //  Created by Marcos on 31/05/25.
 //
 
+import Foundation
+
+struct User: Identifiable, Encodable, Decodable {
+    let id: UUID
+    let email: String
+    let password: String
+    let fullName: String
+    let balance: Double
+    let averageStars: Double
+    
+    init(id: UUID = UUID(), email: String, password: String, fullName: String, balance: Double, averageStars: Double) {
+        self.id = id
+        self.email = email
+        self.password = password
+        self.fullName = fullName
+        self.balance = balance
+        self.averageStars = averageStars
+    }
+}
+
+struct AllUsers: Codable {
+    var users: [User]
+}
