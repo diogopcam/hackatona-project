17ad04ca7ecb5168a133201f38442abd521e8339
Marcos <marcosraach.1@gmail.com>
2025-05-31 01:46:56 -0300

    fix: components

---
+++ hackatona-project/Components/SegmentControl.swift
@@ -0,0 +1,20 @@
+//
+//  SegmentControl.swift
+//  hackatona-project
+//
+//  Created by Marcos on 31/05/25.
+//
+
+import UIKit
+
+class SegmentControl: UISegmentedControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
